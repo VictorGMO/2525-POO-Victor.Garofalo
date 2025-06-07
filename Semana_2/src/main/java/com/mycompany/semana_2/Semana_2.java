86f99731f1a603b6c845777e95c0656ba3bf2477
VictorGMO <vm.garofaloo@uea.edu.ec>
2025-06-07 18:34:11 -0500

    Semana 2 de Tecnicas de Programacion

---
+++ Semana_2/src/main/java/com/mycompany/semana_2/Semana_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana_2;
+
+/**
+ *
+ * @author victo
+ */
+public class Semana_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
